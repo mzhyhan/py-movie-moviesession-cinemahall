4978355ee312e3c881e5c52d2d15723aa705ca30
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2026-02-18 22:04:56 +0000

    Movie, CinemaHall, and MovieSession

--- services/movie.py
+++ services/movie.py
@@ -40,9 +40,9 @@ def create_movie(
                                  description=movie_description)
 
     if genres_ids:
-        movie.genres.add(*genres_ids)
+        movie.genres.set(genres_ids)
 
     if actors_ids:
-        movie.actors.add(*actors_ids)
+        movie.actors.set(actors_ids)
 
     return movie
